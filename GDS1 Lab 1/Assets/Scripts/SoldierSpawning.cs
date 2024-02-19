0f282daa94551103e59f367d9a211393fd3d6653
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2024-02-19 17:05:36 +1100

    Soldier Spawning at Game Start
    Created a Soldier PreFab and a script that randomly positions them at the start of the game.

---
+++ GDS1 Lab 1/Assets/Scripts/SoldierSpawning.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierSpawning : MonoBehaviour
+{
+
+    [SerializeField] GameObject Soldier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            //Spawn soldier in random position
+            Vector2 RandomPosition = new Vector2(Random.Range(-2.0f, 8.5f), Random.Range(-5.0f, 5.0f));
+            GameObject newSoldier = Instantiate(Soldier);
+            newSoldier.transform.position = RandomPosition;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

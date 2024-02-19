4416088e55844f4d0c41bb99af509ef507d3dc14
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2024-02-19 17:31:29 +1100

    Soldier Pickup
    Detects collisions with the soldier and picks up if not at maximum capacity.

--- GDS1 Lab 1/Assets/Scripts/PlayerController.cs
+++ GDS1 Lab 1/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float movementSpeed = 10;
+    int MaxCarryingCapacity = 3;
+    int SoldiersCarrying = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
         Vector2 newDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.Translate(newDirection * movementSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision occurred");
+        if (collision.gameObject.tag == "Soldier" && (SoldiersCarrying < MaxCarryingCapacity))
+        {
+            Destroy(collision.gameObject);
+            SoldiersCarrying += 1;
+            Debug.Log("Soldier Picked up");
+        }
     }
 }
